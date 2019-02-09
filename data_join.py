3d30ee5167d12fa9a1429e3c561b07cb9034f312
jrosenso <jrosenso@umich.edu>
2019-02-09 13:48:11 -0500

    jsut trying to leave my brnach

--- data_join.py
+++ data_join.py
@@ -7,4 +7,6 @@ who_df_new = who_df.loc[who_df['year'].isin([1985, 1986, 2015, 2016])]
 data_df = pd.concat([master_df, who_df_new], sort=True)
 data_df.sort_values('year')
 
-data_df.to_csv('joinedkaggledata.csv', index=False)
+# data_df.to_csv('joinedkaggledata.csv', index=False)
+
+print(data_df.age.unique())
